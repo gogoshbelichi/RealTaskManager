4f55ad3d8117f2cb8ea5df448214ae1029b40dc0
Ivan Nesterov <85916405+gogoshbelichi@users.noreply.github.com>
2025-07-12 13:11:19 +0300

    endpoints moved to Endpoints/AuthEndpoints.cs

--- RealTaskManager.GraphQL/Program.cs
+++ RealTaskManager.GraphQL/Program.cs
@@ -1,12 +1,10 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.IdentityModel.Tokens;
 using RealTaskManager.Core.Entities;
+using RealTaskManager.GraphQL.AuthEndpoints;
 using RealTaskManager.GraphQL.Configurations;
 using RealTaskManager.GraphQL.Extensions;
-using RealTaskManager.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -85,66 +83,7 @@ app.UseAuthorization();
 
 app.MapGraphQL();
 app.MapIdentityApi<TaskManagerUser>();
-
-app.MapPost("/custom-login", async (
-    LoginRequest request,
-    TokenGenerator generator,
-    UserManager<TaskManagerUser> userManager) =>
-{
-    var user = await userManager.FindByEmailAsync(request.Email);
-    if (user is null) return Results.BadRequest("User not found");
-    if (!await userManager.CheckPasswordAsync(user, request.Password))
-        return Results.Unauthorized();
-    var userRoles = await userManager.GetRolesAsync(user);
-    var userId = user.Id;
-    var access_token = generator.GenerateToken(request.Email, userId, userRoles);
-    return Results.Ok(access_token);
-});
-
-app.MapPost("/custom-register", async (
-    UserManager<TaskManagerUser> userManager,
-    RealTaskManagerDbContext dbContext,
-    RegisterRequest request, string username) =>
-{
-    var user = new TaskManagerUser { Email = request.Email,  UserName = username };
-    var result = await userManager.CreateAsync(user, request.Password);
-    if (!result.Succeeded)
-    {
-        return Results.BadRequest($"Registration failed {result.Errors}");
-    }
-
-    var userEntity = new UserEntity
-    {
-        IdentityId = user.Id
-    };
-    
-    await dbContext.Users.AddAsync(userEntity);
-    await dbContext.SaveChangesAsync();
-    
-    return Results.Created();
-});
-
-app.MapPost("/add-role", async (
-    string role,
-    RoleManager<IdentityRole> roleManager) =>
-{
-    if (await roleManager.RoleExistsAsync(role)) return Results.Conflict("Role already exists");
-    var result = await roleManager.CreateAsync(new IdentityRole(role));
-    return result.Succeeded ? Results.Created() : Results.BadRequest("Role not created");
-}).RequireAuthorization("AdminPolicy");
-
-app.MapPatch("/assign-role", async (
-    string role,
-    string email,
-    UserManager<TaskManagerUser> userManager
-    ) =>
-{
-    var user = await userManager.FindByEmailAsync(email);
-    
-    if (user is null) return Results.BadRequest("User not found");
-    var result = await userManager.AddToRoleAsync(user, role);
-    return result.Succeeded ? Results.Accepted() : Results.BadRequest("Role not added");
-}).RequireAuthorization();
+app.MapAuthEndpoints();
 
 app.MapGet("/weather", () => "The weather is nice").RequireAuthorization("UserPolicy");
 
