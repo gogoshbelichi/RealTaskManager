bca926e83d058317079c974be50adefff1aaa82c
Ivan Nesterov <85916405+gogoshbelichi@users.noreply.github.com>
2025-08-08 14:25:52 +0300

    commented for future feature

--- RealTaskManager.Core/Entities/Outbox/OutboxUser.cs
+++ RealTaskManager.Core/Entities/Outbox/OutboxUser.cs
@@ -1,5 +1,10 @@
 namespace RealTaskManager.Core.Entities.Outbox;
-
+// I thought I needed it before for transactional outbox pattern
+// because of 2 dbContexts(but it seems antipattern in scope of one service)
+// for UserProfiles and AspNetUsers
+// but for now it is obsolete, btw we can use it
+// when we will have to fetch with some-kind outside user service and continue developing
+// this feature in future
 public sealed class OutboxUser
 {
     public Guid Id { get; init; }
