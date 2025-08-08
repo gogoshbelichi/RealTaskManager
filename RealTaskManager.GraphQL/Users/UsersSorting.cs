d5a1768896b9881405e01c35068f471d66af0296
Ivan Nesterov <85916405+gogoshbelichi@users.noreply.github.com>
2025-08-09 01:35:16 +0300

    sorting for users

---
+++ RealTaskManager.GraphQL/Users/UsersSorting.cs
@@ -0,0 +1,16 @@
+using HotChocolate.Data.Sorting;
+using RealTaskManager.Core.Entities;
+
+namespace RealTaskManager.GraphQL.Users;
+
+public class UsersSorting : SortInputType<UserEntity> 
+{
+    protected override void Configure(ISortInputTypeDescriptor<UserEntity> descriptor)
+    {
+        descriptor.BindFieldsExplicitly();
+
+        descriptor.Field(x => x.Id);
+        descriptor.Field(x => x.Username);
+        descriptor.Field(x => x.Email);
+    }
+}
