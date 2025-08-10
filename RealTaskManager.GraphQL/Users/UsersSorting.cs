6041c16265241cd5dd6939e1da1c0ac8f6cbd0a3
Ivan Nesterov <85916405+gogoshbelichi@users.noreply.github.com>
2025-08-10 03:31:31 +0300

    fix for ordering close issue

--- RealTaskManager.GraphQL/Users/UsersSorting.cs
+++ RealTaskManager.GraphQL/Users/UsersSorting.cs
@@ -3,7 +3,7 @@ using RealTaskManager.Core.Entities;
 
 namespace RealTaskManager.GraphQL.Users;
 
-public class UsersSorting : SortInputType<UserEntity> 
+public sealed class UsersSorting : SortInputType<UserEntity> 
 {
     protected override void Configure(ISortInputTypeDescriptor<UserEntity> descriptor)
     {
