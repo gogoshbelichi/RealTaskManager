df592f5e6182fafa559bbe630b9e49dfcb89c353
Ivan Nesterov <85916405+gogoshbelichi@users.noreply.github.com>
2025-08-13 05:02:04 +0300

    returned accidentally deleted field -> DateTimeOffset LastAssignedAt

--- RealTaskManager.Core/Entities/TasksAssignedToUser.cs
+++ RealTaskManager.Core/Entities/TasksAssignedToUser.cs
@@ -1,5 +1,7 @@
 namespace RealTaskManager.Core.Entities;
-
+// decided to make many-to-many,
+// for example in future I want to have subtasks and some assignee have to take one subtask
+// and some other assignee can take some other subtask
 public sealed class TasksAssignedToUser
 {
     public Guid TaskId { get; init; }
@@ -9,4 +11,6 @@ public sealed class TasksAssignedToUser
     public Guid UserId { get; init; }
 
     public UserEntity User { get; init; } = null!;
+    
+    public required DateTimeOffset LastAssignedAt { get; set; }
 }
