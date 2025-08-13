69aaa8fe37869701b9a5bf0211bb4b1865ce7053
Ivan Nesterov <85916405+gogoshbelichi@users.noreply.github.com>
2025-08-13 04:57:33 +0300

    exceptions becomes errors for FieldResult<TResult, TError...>

--- RealTaskManager.GraphQL/Tasks/TaskExceptions.cs
+++ RealTaskManager.GraphQL/Tasks/TaskExceptions.cs
@@ -2,6 +2,7 @@ namespace RealTaskManager.GraphQL.Tasks;
 
 public sealed class TitleEmptyException() : Exception("The title cannot be empty.");
 
-public sealed class TaskNotFoundException() : Exception("The task cannot be found.");
+public sealed class UsersNotFoundException(string targetUsername) : Exception("Some users were not found");
 
-public sealed class UserNotFoundException() : Exception("User not found");
+public sealed class PermissionException() :  Exception("You are not allowed");
+
